6b6cbfe577bf117c22e3295d26850a8d28099632
Olafyii <804093559@qq.com>
2020-04-30 17:10:26 +0800

    one pass

--- 1-two-sum.cpp
+++ 1-two-sum.cpp
@@ -3,20 +3,30 @@ public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		map<int, int> idx;
 		map<int, int>::iterator it;
-		vector<int> ret(2, 0);
 		for (int i = 0; i < nums.size(); ++i)
 			idx[nums[i]] = i;
 		for (int i = 0; i < nums.size(); ++i) {
             // printf("i: %d nums[i]: %d idx[nums[i]]: %d\n", i, nums[i], idx[nums[i]]);
 			it = idx.find(target - nums[i]);
-			if (it != idx.end()) {
-                if (i != it->second){
-                    ret[0] = i;
-				    ret[1] = it->second;
-                    return ret;   
-                }
+			if (it != idx.end() && i != it->second) {
+                    return {i, it->second};
 			}
 		}
-        return ret;  // won't be reached
+        return vector<int>();
+	}
+};
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> idx;
+		map<int, int>::iterator it;
+		for (int i = 0; i < nums.size(); ++i){
+            if (idx.find(target-nums[i]) != idx.end()){
+                return {i, idx[target-nums[i]]};
+            }
+            idx[nums[i]] = i;
+        }
+        return vector<int>();
 	}
 };
